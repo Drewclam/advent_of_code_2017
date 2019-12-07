6acf51b757f102756f60435c4158961a873b26c7
Andrew Lam <drewclam@hotmail.com>
2019-12-07 10:12:52 -0500

    Add day2 solution

---
+++ 2019/day_2/day_2.js
@@ -0,0 +1,30 @@
+// --- Day 2: 1202 Program Alarm-- -
+
+// Opcode 1 adds together numbers read from two positions and stores the result in a third position. The three integers immediately after the opcode tell you these three positions - the first two indicate the positions from which you should read the input values, and the third indicates the position at which the output should be stored.
+
+// Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them. Again, the three integers after the opcode indicate where the inputs and outputs are, not their values.
+
+const runOpCode1 = (arr, pos1, pos2, pos3) =>
+  arr.map((val, i, array) => (i === pos3 ? array[pos1] + array[pos2] : val));
+const runOpCode2 = (arr, pos1, pos2, pos3) =>
+  arr.map((val, i, array) => (i === pos3 ? array[pos1] * array[pos2] : val));
+
+const restoreLastState = puzzleInput =>
+  puzzleInput.map((num, i) => {
+    if (i === 1) return 12;
+    if (i === 2) return 2;
+    return num;
+  });
+
+const readIntCode = list => {
+  for (let i = 0; i < list.length; i += 4) {
+    if (list[i] === 1) {
+      list = runOpCode1(list, list[i + 1], list[i + 2], list[i + 3]);
+    } else if (list[i] === 2) {
+      list = runOpCode2(list, list[i + 1], list[i + 2], list[i + 3]);
+    }
+  }
+  return list;
+};
+
+module.exports = { runOpCode1, runOpCode2, readIntCode, restoreLastState };
