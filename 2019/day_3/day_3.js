10de8d17204c102a7cf3e00bbb4d806c073cc457
andrew.lam@rangle.io <andrew.lam@rangle.io>
2019-12-27 22:34:56 -0500

    Solve day3 part 1

--- 2019/day_3/day_3.js
+++ 2019/day_3/day_3.js
@@ -1,74 +1,100 @@
-const parseInput = input =>
-  input.split("\n").map(wirePaths => wirePaths.split(","));
+const parseInput = input => input.split('\n').map(wirePaths => wirePaths.split(','));
 
-const calculateManhattan = ([p1, p2], [q1, q2]) =>
-  Math.abs(p1 - q1) + Math.abs(p2 - q2);
+const calculateManhattan = ([p1, p2], [q1, q2] = [0, 0]) => Math.abs(p1 - q1) + Math.abs(p2 - q2);
 
-const parseInstruction = (coord, instruction) => {
+const getEndCoord = (coord, instruction) => {
   let [direction, ...units] = instruction;
-  units = Number(units.join(""));
+  units = Number(units.join(''));
 
   switch (direction) {
-    case "R":
+    case 'R':
       return [coord[0] + units, coord[1]];
-    case "L":
+    case 'L':
       return [coord[0] - units, coord[1]];
-    case "U":
+    case 'U':
       return [coord[0], coord[1] + units];
-    case "D":
+    case 'D':
       return [coord[0], coord[1] - units];
   }
 };
 
-const parseInstructions = instructions => {
-  return instructions.reduce(
+const getRanges = instructions =>
+  instructions.reduce(
     (coords, instruction) => {
-      const newCoords = parseInstruction(
-        coords[coords.length - 1],
-        instruction
-      );
+      const newCoords = getEndCoord(coords[coords.length - 1], instruction);
       coords.push(newCoords);
       return coords;
     },
-    [[0, 0]]
+    [[0, 0]],
   );
+
+const isWithinRange = (num, rangeMin, rangeMax) => {
+  if (num <= rangeMax && num >= rangeMin) {
+    return true;
+  }
+
+  return false;
 };
 
 const getIntersection = (range1, range2) => {
-  console.log(range1, range2);
-  if (!range1 || !range2) {
-    return [];
-  }
   const [[x1, y1], [x2, y2]] = range1;
   const [[x3, y3], [x4, y4]] = range2;
-  if (x1 === x2 && y3 === y4) {
+  //       6,7
+  //        | 5,5
+  // 3,5 ------- 8,5
+  //        |
+  //       6,3
+  if (
+    x1 === x2 &&
+    y3 === y4 &&
+    isWithinRange(x1, Math.min(x3, x4), Math.max(x3, x4)) &&
+    isWithinRange(y3, Math.min(y1, y2), Math.max(y1, y2))
+  ) {
     return [x1, y3];
+  } else if (
+    x3 === x4 &&
+    y1 === y2 &&
+    isWithinRange(x3, Math.min(x1, x2), Math.max(x1, x2)) &&
+    isWithinRange(y1, Math.min(y3, y4), Math.max(y3, y4))
+  ) {
+    return [x3, y1];
   }
-  return [];
+
+  return null;
 };
 
 const getIntersections = (path1, path2) => {
   let intersections = [];
   for (let i = 0; i < path1.length; i++) {
-    for (let k = 0; k < path2.length; k++) {
-      intersections = intersections.concat(
-        getIntersection([path1[i], path1[i + 1]], [path2[k], path2[k + 1]])
-      );
+    const startCoord1 = path1[i];
+    const endCoord1 = path1[i + 1];
+    for (let j = 0; j < path2.length; j++) {
+      const startCoord2 = path2[j];
+      const endCoord2 = path2[j + 1];
+      if (endCoord1 && endCoord2) {
+        const intersection = getIntersection([startCoord1, endCoord1], [startCoord2, endCoord2]);
+        intersections.push(intersection);
+      }
     }
   }
-  return intersections;
+
+  return intersections.filter(Boolean).filter(coord => !(coord[0] === 0 && coord[1] === 0));
 };
 
 const getClosestManhattanPort = (instructions1, instructions2) => {
-  return getIntersections(instructions1, instructions2);
+  const manhattans = getIntersections(
+    getRanges(instructions1),
+    getRanges(instructions2),
+  ).map(intersection => calculateManhattan(intersection));
+  return Math.min(...manhattans);
 };
 
 module.exports = {
   calculateManhattan,
-  parseInstruction,
+  getEndCoord,
   getIntersection,
   parseInput,
   getClosestManhattanPort,
-  parseInstructions,
-  getIntersections
+  getRanges,
+  getIntersections,
 };
--- 2019/day_3/day_3.spec.js
+++ 2019/day_3/day_3.spec.js
@@ -1,134 +1,132 @@
-const day3 = require("./day_3");
-const input = require("./day_3.input");
+const day3 = require('./day_3');
+const input = require('./day_3.input');
+const answer1 = 273;
 
-describe("Day 3", () => {
-  describe("calculateManhattan", () => {
-    it("should calculate", () => {
+describe('Day 3', () => {
+  describe('calculateManhattan', () => {
+    it('should calculate', () => {
       expect(day3.calculateManhattan([1, 1], [1, 1])).toEqual(0);
       expect(day3.calculateManhattan([0, 0], [3, 3])).toEqual(6);
       expect(day3.calculateManhattan([0, 0], [5, 6])).toEqual(11);
     });
   });
 
-  describe("parseInstruction", () => {
-    it("should get the end coordinate", () => {
-      expect(day3.parseInstruction([10, 5], "R100")).toEqual([110, 5]);
-      expect(day3.parseInstruction([5, 7], "U2")).toEqual([5, 9]);
-      expect(day3.parseInstruction([5, 7], "L2")).toEqual([3, 7]);
-      expect(day3.parseInstruction([5, 7], "D2")).toEqual([5, 5]);
+  describe('getEndCoord', () => {
+    it('should get the end coordinate', () => {
+      expect(day3.getEndCoord([10, 5], 'R100')).toEqual([110, 5]);
+      expect(day3.getEndCoord([5, 7], 'U2')).toEqual([5, 9]);
+      expect(day3.getEndCoord([5, 7], 'L2')).toEqual([3, 7]);
+      expect(day3.getEndCoord([5, 7], 'D2')).toEqual([5, 5]);
     });
   });
 
-  describe("getIntersection", () => {
-    it("should get the intersecting coordinate", () => {
+  describe('getIntersection', () => {
+    it('should get the intersecting coordinate', () => {
       expect(
         day3.getIntersection(
           [
-            [5, 1],
-            [5, 10]
+            [8, 5],
+            [3, 5],
           ],
           [
-            [0, 5],
-            [10, 5]
-          ]
-        )
-      ).toEqual([5, 5]);
+            [6, 7],
+            [6, 3],
+          ],
+        ),
+      ).toEqual([6, 5]);
+
+      expect(
+        day3.getIntersection(
+          [
+            [3, 5],
+            [3, 2],
+          ],
+          [
+            [6, 3],
+            [2, 3],
+          ],
+        ),
+      ).toEqual([3, 3]);
     });
 
-    it("should handle parallel lines", () => {
+    it('should handle parallel lines', () => {
       expect(
         day3.getIntersection(
           [
             [0, 0],
-            [10, 0]
+            [10, 0],
           ],
           [
             [0, 5],
-            [10, 5]
-          ]
-        )
-      ).toEqual([]);
+            [10, 5],
+          ],
+        ),
+      ).toEqual(null);
+    });
+
+    it('should return false', () => {
+      expect(
+        day3.getIntersection(
+          [
+            [0, 0],
+            [8, 0],
+          ],
+          [
+            [6, 7],
+            [6, 3],
+          ],
+        ),
+      ).toEqual(null);
     });
   });
 
-  describe("parseInstructions", () => {
-    it("should get range of coordinates", () => {
-      expect(day3.parseInstructions(["R8", "U5", "L5", "D3"])).toEqual([
+  describe('getRanges', () => {
+    it('should get range of coordinates', () => {
+      expect(day3.getRanges(['R8', 'U5', 'L5', 'D3'])).toEqual([
         [0, 0],
         [8, 0],
         [8, 5],
         [3, 5],
-        [3, 2]
+        [3, 2],
       ]);
 
-      expect(day3.parseInstructions(["U7", "R6", "D4", "L4"])).toEqual([
+      expect(day3.getRanges(['U7', 'R6', 'D4', 'L4'])).toEqual([
         [0, 0],
         [0, 7],
         [6, 7],
         [6, 3],
-        [2, 3]
+        [2, 3],
       ]);
     });
   });
 
-  describe.only("getIntersections", () => {
-    it("should get the intersections", () => {
+  describe('getIntersections', () => {
+    it('should get the intersections', () => {
       expect(
         day3.getIntersections(
-          [
-            [0, 0],
-            [8, 0],
-            [8, 5],
-            [3, 5],
-            [3, 2]
-          ],
-          [
-            [0, 0],
-            [0, 7],
-            [6, 7],
-            [6, 3],
-            [2, 3]
-          ]
-        )
-      ).toContain([
-        [3, 3],
-        [5, 5]
-      ]);
+          day3.getRanges(['R8', 'U5', 'L5', 'D3']),
+          day3.getRanges(['U7', 'R6', 'D4', 'L4']),
+        ),
+      ).toEqual(
+        expect.arrayContaining([
+          [3, 3],
+          [6, 5],
+        ]),
+      );
 
       expect(
         day3.getIntersections(
-          [
-            [0, 0],
-            [8, 0],
-            [8, 5],
-            [3, 5],
-            [3, 2]
-          ],
-          [
-            [0, 0],
-            [0, 7],
-            [6, 7],
-            [6, 3],
-            [2, 3]
-          ]
-        )
+          day3.getRanges(['R8', 'U5', 'L5', 'D3']),
+          day3.getRanges(['U7', 'R6', 'D4', 'L4']),
+        ),
       ).toHaveLength(2);
     });
   });
 
-  xdescribe("getClosestManhattanPort", () => {
-    it("should work with example input", () => {
-      expect(
-        day3.getClosestManhattanPort(
-          ["R8", "U5", "L5", "D3"],
-          ["U7", "R6", "D4", "L4"]
-        )
-      ).toEqual(6);
-    });
-
-    it("should solve with puzzle input", () => {
+  describe('Solution', () => {
+    it('should solve part 1', () => {
       const parsed = day3.parseInput(input);
-      expect(day3.getClosestManhattanPort(parsed[0], parsed[1])).toEqual(0);
+      expect(day3.getClosestManhattanPort(parsed[0], parsed[1])).toEqual(answer1);
     });
   });
 });
