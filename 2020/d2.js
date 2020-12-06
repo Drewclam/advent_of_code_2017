4bc70fe0ee2efffde85b2e08f372b1b107727083
Andrew Lam <drewclam@hotmail.com>
2020-12-05 22:23:27 -0500

    add day 2 wip

---
+++ 2020/d2.js
@@ -0,0 +1,33 @@
+// --- Day 2: Password Philosophy ---
+
+const parseLine = (line) => {
+  const sections = line.split(" ");
+  const [min, max] = sections[0].split("-");
+  const letter = sections[1].split(":")[0];
+  const pw = sections[2];
+  return { letter, min: Number(min), max: Number(max), pw };
+};
+
+const parseInput = (input) =>
+  input.split("\n").map((section) => parseLine(section.trim()));
+
+const checkValidity = ({ letter, min, max, pw }) => {
+  // fails abcoiulknc (c{2,9})
+  const regex = new RegExp(`(${letter}{${min},${max}})`);
+  return regex.test(pw);
+};
+
+const numberOfValidPasswords = (parsedInput) =>
+  parsedInput.reduce((sum, parsedLine) => {
+    if (checkValidity(parsedLine)) {
+      sum += 1;
+    }
+    return sum;
+  }, 0);
+
+module.exports = {
+  parseLine,
+  parseInput,
+  numberOfValidPasswords,
+  checkValidity,
+};
---
+++ 2020/d2.spec.js
@@ -0,0 +1,122 @@
+const {
+  parseLine,
+  parseInput,
+  checkValidity,
+  numberOfValidPasswords,
+} = require("./d2");
+const input = require("./d2.input");
+
+describe("d2", () => {
+  describe("parseLine", () => {
+    it("should parse", () => {
+      const sampleLine = `1-3 a: abcde`;
+      const parsedLine = parseLine(sampleLine);
+      expect(parsedLine).toMatchObject({
+        letter: "a",
+        min: 1,
+        max: 3,
+        pw: "abcde",
+      });
+    });
+
+    it("should parse", () => {
+      const sampleLine = `1-3 b: cdefg`;
+      const parsedLine = parseLine(sampleLine);
+      expect(parsedLine).toMatchObject({
+        letter: "b",
+        min: 1,
+        max: 3,
+        pw: "cdefg",
+      });
+    });
+
+    it("should parse", () => {
+      const sampleLine = `2-9 c: ccccccccc`;
+      const parsedLine = parseLine(sampleLine);
+      expect(parsedLine).toMatchObject({
+        letter: "c",
+        min: 2,
+        max: 9,
+        pw: "ccccccccc",
+      });
+    });
+  });
+
+  describe("parseInput", () => {
+    it("should parse", () => {
+      const sampleInput = `1-3 a: abcde
+          1-3 b: cdefg
+          2-9 c: ccccccccc`;
+      const parsedInput = parseInput(sampleInput);
+      expect(parsedInput[0]).toMatchObject({
+        letter: "a",
+        min: 1,
+        max: 3,
+        pw: "abcde",
+      });
+      expect(parsedInput[1]).toMatchObject({
+        letter: "b",
+        min: 1,
+        max: 3,
+        pw: "cdefg",
+      });
+      expect(parsedInput[2]).toMatchObject({
+        letter: "c",
+        min: 2,
+        max: 9,
+        pw: "ccccccccc",
+      });
+    });
+  });
+
+  describe("checkValidity", () => {
+    it("should return true", () => {
+      const sampleLine = `1-3 a: abcde`;
+      const parsedLine = parseLine(sampleLine);
+      expect(checkValidity(parsedLine)).toBeTruthy();
+    });
+
+    it("should return true", () => {
+      const sampleLine = `2-9 c: ccccccccc`;
+      const parsedLine = parseLine(sampleLine);
+      expect(checkValidity(parsedLine)).toBeTruthy();
+    });
+
+    it("should return false", () => {
+      const sampleLine = `1-3 b: cdefg`;
+      const parsedLine = parseLine(sampleLine);
+      expect(checkValidity(parsedLine)).toBeFalsy();
+    });
+
+    it("should return false", () => {
+      const sampleLine = `17-18 m: lmmmmmmmmmmmmmmmgh`;
+      const parsedLine = parseLine(sampleLine);
+      expect(checkValidity(parsedLine)).toBeFalsy();
+    });
+
+    it("should return true", () => {
+      const sampleLine = `9-19 x: xxxxxxxxxxxxxxxxxxqx`;
+      const parsedLine = parseLine(sampleLine);
+      expect(checkValidity(parsedLine)).toBeTruthy();
+    });
+
+    it("should return false", () => {
+      const sampleLine = `9-13 q: fqxlqsqlqgxkqvrfqgb`;
+      const parsedLine = parseLine(sampleLine);
+      expect(checkValidity(parsedLine)).toBeFalsy();
+    });
+  });
+
+  it("should solve example 1", () => {
+    const sampleInput = `1-3 a: abcde
+    1-3 b: cdefg
+    2-9 c: ccccccccc`;
+    const parsedInput = parseInput(sampleInput);
+    expect(numberOfValidPasswords(parsedInput)).toEqual(2);
+  });
+
+  it("should solve part 1", () => {
+    const parsedInput = parseInput(input);
+    expect(numberOfValidPasswords(parsedInput)).toEqual(1);
+  });
+});
