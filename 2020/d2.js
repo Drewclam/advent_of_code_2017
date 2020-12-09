b100f9dfe27d7dae5292ab28b68179c09005dbc7
andrew.lam@rangle.io <andrew.lam@rangle.io>
2020-12-09 17:37:30 -0500

    Solve d2 part 2

--- 2020/d2.js
+++ 2020/d2.js
@@ -1,18 +1,16 @@
 // --- Day 2: Password Philosophy ---
 
 const parseLine = (line) => {
-  const sections = line.split(" ");
-  const [min, max] = sections[0].split("-");
-  const letter = sections[1].split(":")[0];
+  const sections = line.split(' ');
+  const [min, max] = sections[0].split('-');
+  const letter = sections[1].split(':')[0];
   const pw = sections[2];
   return { letter, min: Number(min), max: Number(max), pw };
 };
 
-const parseInput = (input) =>
-  input.split("\n").map((section) => parseLine(section.trim()));
+const parseInput = (input) => input.split('\n').map((section) => parseLine(section.trim()));
 
 const checkValidity = ({ letter, min, max, pw }) => {
-  // fails abcoiulknc (c{2,9})
   const chars = pw.split('');
   let count = 0;
   for (let i = 0; i < chars.length; i++) {
@@ -26,8 +24,22 @@ const checkValidity = ({ letter, min, max, pw }) => {
   if (count >= min) {
     return true;
   }
+  // fails abcoiculknc (c{2,9})
   // const regex = new RegExp(`(${letter}{${min},${max}})`);
   // return regex.test(pw);
+};
+
+const checkValidity2 = ({ letter, min: index1, max: index2, pw }) => {
+  // incoming indices are base 1
+  let isMatch = false;
+  if (pw[index1 - 1] === letter) {
+    isMatch = true;
+  }
+  if (pw[index2 - 1] === letter) {
+    isMatch = !isMatch;
+  }
+
+  return isMatch;
 };
 
 const numberOfValidPasswords = (parsedInput) =>
@@ -38,9 +50,19 @@ const numberOfValidPasswords = (parsedInput) =>
     return sum;
   }, 0);
 
+const numberOfValidPasswords2 = (parsedInput) =>
+  parsedInput.reduce((sum, parsedLine) => {
+    if (checkValidity2(parsedLine)) {
+      sum += 1;
+    }
+    return sum;
+  }, 0);
+
 module.exports = {
   parseLine,
   parseInput,
   numberOfValidPasswords,
   checkValidity,
+  checkValidity2,
+  numberOfValidPasswords2,
 };
--- 2020/d2.spec.js
+++ 2020/d2.spec.js
@@ -1,4 +1,11 @@
-const { parseLine, parseInput, checkValidity, numberOfValidPasswords } = require('./d2');
+const {
+  parseLine,
+  parseInput,
+  checkValidity,
+  checkValidity2,
+  numberOfValidPasswords,
+  numberOfValidPasswords2,
+} = require('./d2');
 const input = require('./d2.input');
 
 describe('d2', () => {
@@ -114,6 +121,41 @@ describe('d2', () => {
     it('should solve part 1', () => {
       const parsedInput = parseInput(input);
       expect(numberOfValidPasswords(parsedInput)).toEqual(536);
+    });
+  });
+
+  describe('checkValidity2', () => {
+    it('should return true', () => {
+      const sampleLine = `1-3 a: abcde`;
+      const parsedLine = parseLine(sampleLine);
+      expect(checkValidity2(parsedLine)).toBe(true);
+    });
+
+    it('should return false', () => {
+      const sampleLine = `1-3 b: cdefg`;
+      const parsedLine = parseLine(sampleLine);
+      expect(checkValidity2(parsedLine)).toBe(false);
+    });
+
+    it('should return false', () => {
+      const sampleLine = `2-9 c: ccccccccc`;
+      const parsedLine = parseLine(sampleLine);
+      expect(checkValidity2(parsedLine)).toBe(false);
+    });
+  });
+
+  describe('numberOfValidPaasswords2', () => {
+    it('should solve example 2', () => {
+      const sampleInput = `1-3 a: abcde
+      1-3 b: cdefg
+      2-9 c: ccccccccc`;
+      const parsedInput = parseInput(sampleInput);
+      expect(numberOfValidPasswords2(parsedInput)).toEqual(1);
+    });
+
+    it('should solve part 2', () => {
+      const parsedInput = parseInput(input);
+      expect(numberOfValidPasswords2(parsedInput)).toEqual(558);
     });
   });
 });
